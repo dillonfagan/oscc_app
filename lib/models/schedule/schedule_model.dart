2158b4ac28fd781537ba74c1539ed9b224ec0cb3
Dillon Fagan <dillonfagan@pm.me>
2022-05-06 16:25:39 -0400

    Skip previous rides

--- lib/models/schedule/schedule_model.dart
+++ lib/models/schedule/schedule_model.dart
@@ -8,6 +8,14 @@ class ScheduleModel extends ChangeNotifier {
         "https://script.google.com/macros/s/AKfycbzfZLZvP_mRFP9id1FOd-XG50k9IC4BcYfo0QYJgyTeucDfeA-OYk5XOqA_aKpgXVIk/exec"));
 
     if (response.statusCode != 200) throw "Unable to retrieve ride schedule.";
-    return Ride.decodeList(response.body);
+    Iterable<Ride> rides = Ride.decodeList(response.body);
+
+    if (skipPastRides) {
+      rides = rides.where((ride) => ride.date!.isAfter(DateTime.now()));
+    }
+
+    return rides;
   }
+
+  static bool skipPastRides = true;
 }
