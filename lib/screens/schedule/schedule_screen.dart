b37f3bc85a05fc8a51f60c246d0f98b132dfa783
Dillon Fagan <dillonfagan@pm.me>
2022-05-16 21:23:26 -0400

    Introduce RefreshIndicator

--- lib/screens/schedule/schedule_screen.dart
+++ lib/screens/schedule/schedule_screen.dart
@@ -1,6 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:oscc_app/models/schedule/schedule_model.dart';
-import 'package:provider/provider.dart';
 import '../../models/schedule/ride.dart';
 import 'ride_card.dart';
 
@@ -12,38 +12,45 @@ class ScheduleScreen extends StatefulWidget {
 }
 
 class _ScheduleState extends State<ScheduleScreen> {
+  static final _api = RidesAPI();
+  Future<Iterable<Ride>> _rides = _api.getAll();
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<ScheduleModel>(
-      builder: (context, schedule, child) {
-        return Scaffold(
-          body: StreamBuilder<Iterable<Ride>>(
-              stream: schedule.getAll().asStream(),
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return Center(child: Text(snapshot.error.toString()));
-                }
+    return Scaffold(
+      body: StreamBuilder<Iterable<Ride>>(
+        stream: _rides.asStream(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          }
 
-                if (!snapshot.hasData) {
-                  return const Center(
-                      child: CircularProgressIndicator(color: Colors.red));
-                }
+          if (!snapshot.hasData) {
+            return const Center(
+                child: CircularProgressIndicator(color: Colors.blue));
+          }
 
-                final rides = snapshot.requireData.toList();
+          final rides = snapshot.requireData.toList();
 
-                if (rides.isEmpty) {
-                  return const Center(child: Text("No Rides Scheduled"));
-                }
+          if (rides.isEmpty) {
+            return const Center(child: Text("No Rides Scheduled"));
+          }
 
-                return ListView.separated(
-                  itemCount: rides.length,
-                  itemBuilder: (context, index) => RideCard(rides[index]),
-                  separatorBuilder: (context, index) =>
-                      const SizedBox(height: 8.0),
-                );
-              }),
-        );
-      },
+          return RefreshIndicator(
+            onRefresh: _refresh,
+            child: ListView.separated(
+              itemCount: rides.length,
+              itemBuilder: (context, index) => RideCard(rides[index]),
+              separatorBuilder: (context, index) => const SizedBox(height: 8.0),
+              physics: const AlwaysScrollableScrollPhysics(),
+            ),
+          );
+        },
+      ),
     );
+  }
+
+  Future<void> _refresh() async {
+    setState(() => _rides = _api.getAll());
   }
 }
